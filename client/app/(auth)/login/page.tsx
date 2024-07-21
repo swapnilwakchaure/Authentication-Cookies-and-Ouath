f8213fbd5c500b252b86818a620db8ce376093dc
swapnilwakchaure <swapnilwakchaure2411999@gmail.com>
2024-07-21 23:50:40 +0530

    register-login-page-raw-design-done

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,13 +1,33 @@
 import Link from "next/link";
 
-export default function LoginPage() {
+export default function Login() {
   return (
-    <div className="flex justify-center items-center">
-      <div>
-        <h1>Login Page</h1>
-        <Link href="/register" className="underline text-blue-600">
-          Don't have an account
-        </Link>
+    <div className="flex justify-center items-center mt-20">
+      <div className="border p-4 rounded shadow-2xl">
+        <h1 className="text-md">Login Page</h1>
+        <form action="">
+          <input
+            type="email"
+            autoFocus
+            className="border rounded my-2 pl-2"
+            placeholder="Email"
+          />
+          <br />
+          <input
+            type="password"
+            className="border rounded my-2 pl-2"
+            placeholder="Password"
+          />
+          <br />
+          <button className="border rounded hover:border-sky-900 my-2 w-full bg-sky-900 text-white hover:text-sky-900 hover:bg-white">
+            Login
+          </button>
+        </form>
+        <div className="flex justify-end text-sm">
+          <Link href="/register" className="underline text-blue-600">
+            Create an account
+          </Link>
+        </div>
       </div>
     </div>
   );
--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -2,10 +2,21 @@ import Link from "next/link";
 
 export default function Register() {
   return (
-    <div className="flex justify-center items-center">
-      <div>
-        <h1>Register Page</h1>
-        <Link href="/login" className="underline text-blue-600">Already registerd</Link>
+    <div className="flex justify-center items-center mt-20">
+      <div className="border p-4 rounded shadow-2xl">
+        <h1 className="text-md">Register Page</h1>
+        <form action="">
+          <input type="text" autoFocus className="border rounded my-2 pl-2" placeholder="Full Name" />
+          <br />
+          <input type="email" className="border rounded my-2 pl-2" placeholder="Email" />
+          <br />
+          <input type="password" className="border rounded my-2 pl-2" placeholder="Password" />
+          <br />
+          <button className="border rounded hover:border-sky-900 my-2 w-full bg-sky-900 text-white hover:text-sky-900 hover:bg-white">Register</button>
+        </form>
+        <div className="flex justify-end text-sm">
+          <Link href="/login" className="underline text-blue-600">Already registerd</Link>
+        </div>
       </div>
     </div>
   );
